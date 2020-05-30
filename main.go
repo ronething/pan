199b02f2d88c4270c66f569f70cf07fdbc4f98ea
ronething <axingfly@gmail.com>
2020-05-30 12:28:04 +0800

    fix: comment

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/file/upload", handler.UploadHandler)
-	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
-	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
-	http.HandleFunc("/file/query", handler.FileQueryHandler)
-	http.HandleFunc("/file/download", handler.DownloadHandler)
-	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
-	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
+	http.HandleFunc("/file/upload", handler.UploadHandler)         // 上传
+	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)  // 上传成功提示
+	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)      // 获取元数据
+	http.HandleFunc("/file/query", handler.FileQueryHandler)       // 批量获取元数据
+	http.HandleFunc("/file/download", handler.DownloadHandler)     // 下载
+	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler) // 重命名
+	http.HandleFunc("/file/delete", handler.FileDeleteHandler)     // 删除
 	err := http.ListenAndServe("127.0.0.1:8887", nil)
 	if err != nil {
 		fmt.Printf("Failed to start server, err: %s", err.Error())
